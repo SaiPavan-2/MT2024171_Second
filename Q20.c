3dc87922fb88c26b04bdfa608c1cab558d53c9cd
SaiPavan-2 <yellumahanthysaipavan@gmail.com>
2024-09-19 19:19:48 +0530

    fourth

---
+++ Q20.c
@@ -0,0 +1,26 @@
+/**
+Name:Q20.c
+Author:Pavan
+Description:Two programs so that both can communicate by FIFO -Use one way communication.
+Date:19th September 2024
+**/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+    int fd2 = open("myfifo2", O_RDONLY);
+    char buff[100];
+    
+    read(fd2, buff, sizeof(buff));
+    printf("Received data: %s\n", buff);
+    
+    close(fd2);
+    return 0;
+}
+/**
+Output:Received data: Hello World
+**/
+
